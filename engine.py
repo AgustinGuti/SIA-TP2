9240203aa1f95d6e45aca1020f46356e1bcca241
jeremias.feferovich <jeremias.feferovich@mendel.com>
2024-04-02 19:13:10 -0300

    create result.py
    Co-authored-by: Agustin Andres Gutierrez <AgustinGuti@users.noreply.github.com>

--- engine.py
+++ engine.py
@@ -26,10 +26,11 @@ class AlgorithmData:
         return str(self)
 
 class AlgorithmConfig:
-    def __init__(self, population_size, class_name, selection_config_a: SelectionConfig, selection_config_b: SelectionConfig, selection_config_rate,
+    def __init__(self, population_size, population_to_keep, class_name, selection_config_a: SelectionConfig, selection_config_b: SelectionConfig, selection_config_rate,
                  crossover_config: CrossoverConfig, mutation_config: MutationConfig, replacement_config_a: ReplacementConfig, replacement_config_b: ReplacementConfig,
                  replacement_config_rate, end_condition_config: EndConditionConfig):
         self.population_size = population_size
+        self.population_to_keep = population_to_keep
         self.class_name = class_name
         self.selection_config_a = selection_config_a
         self.selection_config_b = selection_config_b
@@ -44,6 +45,7 @@ class AlgorithmConfig:
     def json(self):
         return {
             "population_size": self.population_size,
+            "population_to_keep": self.population_to_keep,
             "class_name": self.class_name,
             "selection_config_a": self.selection_config_a.json(),
             "selection_config_b": self.selection_config_b.json(),
@@ -135,70 +137,57 @@ def algorithm(population_to_keep, hard_cap, config: AlgorithmConfig):
         json.dump(json_data, file, indent=4)
 
     return population, best
-      
-    return population, best
 
-    # current_best: Character = max(population, key=lambda x: x.performance)
-    # algorithm_data = AlgorithmData(population, (current_best, 0))
-    # best = current_best, algorithm_data.generation
-    # while not should_end(algorithm_data.generation, algorithm_data.population, algorithm_data.best[0], config.end_condition_config) and algorithm_data.generation < hard_cap:
-    #     algorithm_data.generation += 1
-    #     algorithm_data.population = algorithm_iteration(algorithm_data, population_to_keep, config)
-    #     current_best = max(algorithm_data.population, key=lambda x: x.performance)
-    #     algorithm_data.last_bests.append((current_best, algorithm_data.generation))
-    #     if current_best.performance > algorithm_data.best[0].performance:
-    #         algorithm_data.best = current_best, algorithm_data.generation
-    #         best = algorithm_data.best
-    #     # with open("log.txt", "a") as file:
-    #     #     file.write(f"Iteration: {algorithm_data.generation}, best: {current_best}, BEST: {current_best.performance:.2f}, AVG: {np.mean([x.performance for x in population]):.2f}, HSTD: {np.std([x.variables.height for x in population])} \n")  
-    
-    # i = 0
-    # filename = f"./results/result_{i}.json"
-    # while os.path.exists(filename):
-    #     i += 1
-    #     filename = f"./results/result_{i}.json"
-    # os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-    # json_data = {
-    #     "results": {
-    #         "best": {
-    #             "solution": algorithm_data.best[0].json(),
-    #             "generation": algorithm_data.best[1]
-    #         },
-    #         "last_bests": [{"character":x[0].json(), "generation": x[1]} for x in algorithm_data.last_bests],
-    #         "last_generation": algorithm_data.generation
-    #     },
-    #     "config": config.json()
-    # }
-    # with open(filename, "w") as file:
-    #     json.dump(json_data, file, indent=4)
-
-    # return population, best
-
-def main():
-    with open("config.yaml", "r") as file:
-        config = yaml.load(file, Loader=yaml.FullLoader)
-
+def build_config(config):
     population_to_keep = config['algorithm_config']["population_to_keep"]
     population_size = config['algorithm_config']["population_size"]
 
-    first_selection = round(config["selection_config"]["ratio_A"]*population_size)
-    second_selection = population_size - first_selection
+    first_selection = round(config["selection_config"]["ratio_A"]*population_to_keep)
+    second_selection = population_to_keep - first_selection
     selection_config_a = SelectionConfig(config["selection_config"]["type_1"], first_selection, config["selection_config"]["tournament_size"], config["selection_config"]["tournament_threshold"], config["selection_config"]["initial_temperature"], config["selection_config"]["temperature_decay"], config["selection_config"]["min_temperature"])
     selection_config_b = SelectionConfig(config["selection_config"]["type_2"], second_selection, config["selection_config"]["tournament_size"], config["selection_config"]["tournament_threshold"], config["selection_config"]["initial_temperature"], config["selection_config"]["temperature_decay"], config["selection_config"]["min_temperature"])
     crossover_config = CrossoverConfig(config["crossover_config"]["type"])
     mutation_config = MutationConfig(config["mutation_config"]["type"], config["mutation_config"]["rate"], config["mutation_config"]["delta"])
 
-    first_replacement = round(config["replacement_config"]["ratio_B"]*population_size)
-    second_replacement = population_size - first_replacement
+    first_replacement = round(config["replacement_config"]["ratio_B"]*population_to_keep)
+    second_replacement = population_to_keep - first_replacement
     replacement_config_a = ReplacementConfig(config["replacement_config"]["type_1"], first_replacement, config["replacement_config"]["gen_gap"])
     replacement_config_b = ReplacementConfig(config["replacement_config"]["type_2"], second_replacement, config["replacement_config"]["gen_gap"])
     end_condition_config = EndConditionConfig(config["end_condition_config"]["type"], config["end_condition_config"]["generations_to_check"], config["end_condition_config"]["optimum"], config["end_condition_config"]["tolerance"], config["end_condition_config"]["structure_tolerance"], config["end_condition_config"]["structure_relevant_proportion"], config["end_condition_config"]["generations"])
 
-    algorithm_config = AlgorithmConfig(population_size, config['algorithm_config']["class_name"], selection_config_a, selection_config_b, config["selection_config"]["ratio_A"],
+    algorithm_config = AlgorithmConfig(population_size, population_to_keep, config['algorithm_config']["class_name"], selection_config_a, selection_config_b, config["selection_config"]["ratio_A"],
                                         crossover_config, mutation_config, replacement_config_a, replacement_config_b, config["replacement_config"]["ratio_B"], end_condition_config)
-    result, best = algorithm(population_to_keep, config["algorithm_config"]["hard_cap_iterations"],  algorithm_config)
-    print(f"Best solution: {best[0]} at generation {best[1]}")
+    return algorithm_config, population_to_keep, config["algorithm_config"]["hard_cap_iterations"]
+
+def main():
+    with open("config.yaml", "r") as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    with open("run_config.yaml", "r") as file:
+        run_config = yaml.load(file, Loader=yaml.FullLoader)
+
+    if(run_config["use_default"]):
+        algorithm_config, population_to_keep, hard_cap = build_config(config)
+        result, best = algorithm(population_to_keep, hard_cap,  algorithm_config)
+        print(f"Best solution: {best[0]}")
+    else:
+        if run_config['param_value']['numeric']:
+            min_value = run_config['param_value']['numeric_value']['min_value']
+            max_value = run_config['param_value']['numeric_value']['max_value']
+            step = run_config['param_value']['numeric_value']['step']
+            for i in range(min_value, max_value+step, step):
+                print(f"Iteration {i}")
+                config[run_config['param_to_change_group']][run_config['param_to_change_name']] = i
+                algorithm_config, population_to_keep, hard_cap = build_config(config)
+                result, best = algorithm(population_to_keep, hard_cap,  algorithm_config)
+        elif run_config['param_value']['categorical']:
+            for i in run_config['param_value']['categorical_values']:
+                config[run_config['param_to_change_group']][run_config['param_to_change_name']] = i
+                algorithm_config, population_to_keep, hard_cap = build_config(config)
+                result, best = algorithm(population_to_keep, hard_cap,  algorithm_config)
+                print(f"Iteration {i} finished")
+        else:
+            raise ValueError("Invalid run_config")
+
 
 if __name__ == "__main__":
     main()
---
+++ results.py
@@ -0,0 +1,35 @@
+import json
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+
+def main():
+    matplotlib.use('TkAgg')
+    results = []
+    for filename in glob.glob('results/*.json'):
+        with open(filename, 'r') as f:
+          results.append(json.load(f))
+
+    df = pd.DataFrame(results)
+    df = pd.json_normalize(results)
+    
+
+    plt.figure()   
+    grouped = df.groupby('config.class_name')
+    plt.errorbar(grouped.groups.keys(), grouped['results.best.solution.performance'].mean(), yerr=grouped['results.best.solution.performance'].std(), fmt='o', capsize=6)
+
+    plt.figure()
+    grouped = df.groupby('config.selection_config_a.population_to_keep')
+    plt.errorbar(grouped.groups.keys(), grouped['results.best.solution.performance'].mean(), yerr=grouped['results.best.solution.performance'].std(), fmt='-o', capsize=6)
+
+    plt.figure()
+    grouped = df.groupby('config.population_size')
+    plt.errorbar(grouped.groups.keys(), grouped['results.best.solution.performance'].mean(), yerr=grouped['results.best.solution.performance'].std(), fmt='-o', capsize=6)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
